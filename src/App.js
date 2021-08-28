7b16234aad28eb601aa73984e37a53a2132e2898
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-08-28 19:48:31 +0530

    Dummy Autocomplete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import './custom.css'
 
 function App() {
+  const [text, setText] = useState('')
+  const [user, setuser] = useState([])
+  const [suggestions, setSuggestions] = useState([])
+  useEffect(() => {
+    const loadUser = async () => {
+      const response= await axios.get('https://reqres.in/api/users');
+      setuser(response.data.data)
+    }
+    loadUser();
+  },[])
+
+  const onSuggestHandler = (text) =>{
+    setText(text);
+    setSuggestions([])
+  }
+
+  const onChangeHandler = (text) => {
+    let matches= []
+    if(text.length>0){
+      matches=user.filter(user => {
+        const regex= new RegExp(`${text}`, "gi")
+        return user.email.match(regex)
+      })
+    }
+    setSuggestions(matches)
+    setText(text)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input type="text" className="col-md-12" style={{ marginTop: '10px' }} onChange={ e => onChangeHandler(e.target.value)} value={text} onBlur={() => {
+        setTimeout(() => {
+          setSuggestions([])
+        }, 100);
+      }}/>
+    
+      {suggestions && suggestions.map((suggestion, i) => 
+      <div key={i}  className="suggestion col-md-12 justify-content-md-center" onClick={() => onSuggestHandler(suggestion.email)}>{suggestion.email}</div>
+      )}
+    
     </div>
+    
   );
 }
 
